f78cab199a54d238b13ad993f83b00ae5a1999d6
zhengjw22 <zhengjunweimail@163.com>
2025-01-24 15:57:38 +0800

    更新文档

--- docs/.vuepress/public/js/gitee.js
+++ docs/.vuepress/public/js/gitee.js
@@ -17,8 +17,20 @@ if (typeof window !== 'undefined') {
                     resp = JSON.parse(result)
                     console.log("result", resp);
                     if (resp.access_token != null) {
-                        localStorage.setItem('access_token', resp.access_token);
-                        window.location.href = state + "?timestamp=" + Date.now();
+                        //获取用户信息
+                        localStorage.removeItem('gitee_user');
+                        const token = resp.access_token
+                        fetch("https://gitee.com/api/v5/user?access_token=" + token, {
+                            method: "GET"
+                        }).then(response => response.text())
+                            .then(result => {
+                                resp = JSON.parse(result)
+                                console.log("result", resp);
+                                localStorage.setItem('access_token', token);
+                                localStorage.setItem('gitee_user', result);
+                                window.location.href = state + "?timestamp=" + Date.now();
+                            })
+
                     } else if ("invalid_grant_accessibility" == resp.error) {
                         window.location.href = 'https://gitee.com/oauth/authorize?client_id=' + client_id + '&redirect_uri=' + gitee_redirect_uri + '&response_type=code';
                     }
@@ -27,15 +39,35 @@ if (typeof window !== 'undefined') {
         }
     }
 
+    function getUserInfo() {
+        let access_token = checkAccessToken()
+        if (access_token == null) {
+            return
+        }
+        let user = localStorage.getItem('gitee_user')
+        if (user != null) {
+            return JSON.parse(user)
+        }
+    }
+
+    function checkAccessToken() {
+        let access_token = localStorage.getItem('access_token')
+        console.log("access_token", access_token);
+        if (access_token == null) {
+            window.location.href = 'https://gitee.com/oauth/authorize?client_id=' + client_id + '&redirect_uri=' + gitee_redirect_uri + '&response_type=code&state=' + location.href;
+            return null;
+        } else {
+            return access_token;
+        }
+    }
+
     function checkStar(owner, repo) {
         //文档首页不校验
         if (location.pathname === repo || location.pathname === repo + '/') {
             return;
         }
-        access_token = localStorage.getItem('access_token')
-        console.log("access_token", access_token);
+        let access_token = checkAccessToken()
         if (access_token == null) {
-            window.location.href = 'https://gitee.com/oauth/authorize?client_id=' + client_id + '&redirect_uri=' + gitee_redirect_uri + '&response_type=code&state=' + location.href;
             return
         }
         fetch("https://gitee.com/api/v5/user/starred/" + owner + "/" + repo + "?access_token=" + access_token, {
@@ -49,24 +81,72 @@ if (typeof window !== 'undefined') {
                     checkStar(owner, repo)
                 } else if (response.status == 404) {
                     //替换 html body中 class为 content-wrapper 中的内容,适配vuepress
-                    d = document.querySelector(".content-wrapper")
-                    if (d == null) {
-                        //适配 astro
-                        d = document.querySelector(".main-pane")
-                    }
-                    if (d == null) {
-                        d = document.getElementsByTagName("body")[0]
-                    }
-                    d.innerHTML = `
+                    replaceHtml(`
+                                <div>
                                 检测到您还未 Star 本项目，暂时无法访问本页面。<br/>
                               请先前往：<a target="_blank" href="https://gitee.com/${owner}/${repo}">https://gitee.com/${owner}/${repo}</a> 完成操作，再尝试刷新当前页面。
                                 </div>
-                                `
+                                `)
                 }
             })
             .catch(error => console.error(error));
     }
 
+    function checkAuthorize(owner, repo, file) {
+        let access_token = checkAccessToken()
+        if (access_token == null) {
+            return
+        }
+        let user = getUserInfo()
+        fetch("https://gitee.com/api/v5/repos/" + owner + "/" + repo + "/contents/" + file + "?access_token=" + access_token, {
+            method: "GET"
+        })
+            .then(response => {
+                console.log("check result", response);
+                if (response.status == 200) {
+                    //解析body
+                    response.json().then(function (data) {
+                        console.log("check result", data);
+                        //解析base64,
+                        let content = atob(data.content)
+                        //转成utf8
+                        content = decodeURIComponent(escape(content))
+
+                        console.log("check result", content);
+                        //解析json
+                        let json = JSON.parse(content)
+                        if (json[user?.login] != null) {
+                            console.log("check result", "ok");
+                        } else {
+                            replaceHtml(`<div>
+                              未经授权暂时无法访问该页面。<br/>
+                               </div>
+                                `)
+                        }
+                    });
+                } else {
+                    console.log("check fail result", response)
+                    localStorage.removeItem("access_token")
+                    checkAuthorize(owner, repo, file)
+                }
+            })
+            .catch(error => console.error(error));
+    }
+
+    function replaceHtml(html) {
+        //替换 html body中 class为 content-wrapper 中的内容,适配vuepress
+        d = document.querySelector(".content-wrapper")
+        if (d == null) {
+            //适配 astro
+            d = document.querySelector(".main-pane")
+        }
+        if (d == null) {
+            d = document.getElementsByTagName("body")[0]
+        }
+        d.innerHTML = html
+    }
+
     window.checkStar = checkStar;
     window.redirect = redirect;
+    window.checkAuthorize = checkAuthorize;
 }
